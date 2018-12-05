3a420849b6f37d6406231b43552ad48696802c25
danieljensen96 <danielsj96@gmail.com>
2018-12-05 10:48:48 +0100

    Oprettet spiller model Gametroller rettet Index view opdateret

--- Bordfodbold/Bordfodbold/Controllers/GameController.cs
+++ Bordfodbold/Bordfodbold/Controllers/GameController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bordfodbold.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,5 +14,12 @@ namespace Bordfodbold.Controllers
         {
             return View();
         }
+        List<Spiller> spiller = new List<Spiller> {
+            new Spiller {spiller_navn = "john"},
+            new Spiller {spiller_navn = "patte"},
+            new Spiller {spiller_navn = "glenn"},
+            new Spiller {spiller_navn = "daniel"}
+        
+    };
     }
 }
---
+++ Bordfodbold/Bordfodbold/Models/Spiller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bordfodbold.Models
+{
+    public class Spiller
+    {
+        public int spiller_id { get; set; }
+
+        public string spiller_navn { get; set; }
+
+        public int spiller_mål { get; set; }
+
+        public int spillede_kampe { get; set; }
+
+
+    }
+}
