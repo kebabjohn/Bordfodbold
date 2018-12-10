ad73b71d53c7f0e148e435f7f0ce0d8f8c7e9833
GoofyGlenn <33924992+GoofyGlenn@users.noreply.github.com>
2018-12-10 12:56:52 +0100

    Admin View added

--- Bordfodbold/Bordfodbold/Controllers/AdminController.cs
+++ Bordfodbold/Bordfodbold/Controllers/AdminController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Bordfodbold.Abstract;
+using Bordfodbold.Models;
+
 
 namespace Bordfodbold.Controllers{
 
     public class AdminController : Controller{
+
+
+        private ISpillerRepository repos;
+
+        public AdminController(ISpillerRepository repo) {
+            this.repos = repo;
+        }
+
         // GET: Admin
-        public ActionResult Index(){
+        public ActionResult Admin(){
 
 
-            return View();
+            return View(repos.Spillere);
         }
     }
 }
