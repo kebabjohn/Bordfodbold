ac8318b793b27969e194f05d2fe6103b3dc0a5cb
kebabjohn <32761420+kebabjohn@users.noreply.github.com>
2018-12-13 10:31:57 +0100

    Lavet beginform i index og display name i spiller

--- Bordfodbold/Bordfodbold/Models/Spiller.cs
+++ Bordfodbold/Bordfodbold/Models/Spiller.cs
@@ -12,13 +12,16 @@ namespace Bordfodbold.Models
     [HiddenInput(DisplayValue = false)]
     public int SpillerID { get; set; }
 
+    [Display(Name ="Navn")]
     [Required(ErrorMessage = "Please enter a name")]
     public string Spiller_Name { get; set; }
 
+    [Display(Name ="Mål")]
     [Required]
     [Range(0-100, int.MaxValue, ErrorMessage = "Please enter number of goals")]
     public int Spiller_Maal { get; set; }
 
+    [Display(Name ="Kampe")]
     [Required]
     [Range(0 - 100, int.MaxValue, ErrorMessage = "Please enter number of games played")]
     public int Spiller_Kampe { get; set; }
